4bf900c607d652aa282dcf40765de94e6df2975c
vadim <lavrik-v05@mail.ru>
2023-11-14 23:33:35 +0300

    3 task

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 RED = '\u001b[41m'
 BLUE = '\u001b[44m'
 WHITE = '\u001b[47m'
@@ -31,51 +32,16 @@ def pattern():
             print(f'{WHITE}{"  " * 16}{END}')
 
 
+def chart():
 
+    x = [0, 2, 4, 6, 8, 10]
+    y = [0, 1, 2, 3, 4, 5]
+    plt.plot(x, y)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('y = x / 2')
+    plt.show()
 
-# plot_list = [[0 for i in range(10)] for i in range(10)]
-# result = [0 for i in range(10)]
-
-# for i in range(10):
-#     result[i] = i ** 3
-
-# step = round(abs(result[0] - result[9]) / 9, 2)
-# print(step)
-
-# for i in range(10):
-#     for j in range(10):
-#         if j == 0:
-#             plot_list[i][j] = step * (8-i) + step
-
-# for i in range(9):
-#     for j in range(10):
-#         if abs(plot_list[i][0] - result[9 - j]) < step:
-#             for k in range(9):
-#                 if 8 - k == j:
-#                     plot_list[i][k+1] = 1
-
-# for i in range(9):
-#     line = ''
-#     for j in range(10):
-#         if j == 0:
-#             line += '\t' + str(int(plot_list[i][j])) + '\t'
-#         if plot_list[i][j] == 0:
-#             line += '--'
-#         if plot_list[i][j] == 1:
-#             line += '!!'
-#     print(line)
-# print('\t0\t1 2 3 4 5 6 7 8 9')
-
-# for i in range(10):
-#     #print(plot_list[i])
-#     pass
-
-# file = open('sequence.txt', 'r')
-# list = []
-# for number in file:
-#     list.append(float(number))
-# file.close()
-# print(list)
 
 def numbers():
     with open('sequence.txt') as f:
@@ -88,3 +54,7 @@ def numbers():
                 res.append(j)
         print(res)
 
+flag()
+pattern()
+chart()
+numbers()
